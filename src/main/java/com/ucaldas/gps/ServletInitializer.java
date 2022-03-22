9c65a7a89da0a533871238a7c7261dc81e9c61f8
willy <willington.londono@ucaldas.edu.co>
2022-03-22 11:14:54 -0500

    Commit primer proyecto

---
+++ src/main/java/com/ucaldas/gps/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ucaldas.gps;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GpsApplication.class);
+	}
+
+}
---
+++ src/test/java/com/ucaldas/gps/GpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucaldas.gps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
