2c45f04945a6ff0d961903d46d1d37c43db1ee87
willy <willington.londono@ucaldas.edu.co>
2022-04-05 18:02:09 -0500

    prueba

--- src/main/java/com/ucaldas/gps/GpsApplication.java
+++ src/main/java/com/ucaldas/gps/GpsApplication.java
@@ -15,7 +15,7 @@ public class GpsApplication {
 	}
 	
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public String hello(@RequestParam(value = "name", defaultValue = "World1") String name) {
 		return String.format("Hello %s!", name);
 	}
 
